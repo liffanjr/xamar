8eef5299c50b9f2b58ac2f9b6db119405cded92f
jdrenthe <j.r.o.drenthe@gmail.com>
2019-08-06 16:06:18 +0200

    gulp setup

---
+++ NutriThink/NutriThink/gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp')
+    , sass = require('gulp-sass')
+    , uglify = require("gulp-uglify")
+    , minify = require('gulp-minify');
+
+gulp.task('css',
+    /**
+     * Compiles the sass to css.
+     * @returns {css};
+     * */
+    function () {
+        return gulp.src('./Style/*.scss')
+            .pipe(sass.sync().on('error', sass.logError))
+            .pipe(gulp.dest('./NutriThink/css'));
+    });
+
+gulp.task('watch-sass',
+    /**
+     * Watches for changes in te 'Style' folder.
+     * When a changes has bin saved te 'sass' is called.
+     * */
+    function () {
+        gulp.watch('./Style/**/*.scss', gulp.series('css'));
+    });
+
+// I think this is not deeded
+//gulp.task('js',
+///**
+// * minifys & uglifys the js.
+// * @returns {js.min};
+// * */
+//function () {
+//    return gulp.src([
+//        './Javascript/*.js',
+//        './Javascript/**/*.js'
+//    ])
+//        //.pipe(minify())
+//        //.pipe(uglify())
+//        .pipe(gulp.dest('./wwwroot/js'));
+//});
+//
+//gulp.task('watch-js',
+//    /**
+//     * Watches for changes in te 'Javascript' folder.
+//     * When a changes has bin saved te 'js' is called.
+//     * */
+//    function () {
+//        gulp.watch('./Javascript/**/*.js', gulp.series('js'));
+//    });
